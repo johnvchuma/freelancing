bf0d11074eaa82a64da82117572aff0d382a36ca
johnvchuma <johnvchuma@gmail.com>
2022-11-24 17:17:34 +0300

    save changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/me/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/freelancing/deploy.log
 
-echo '/home/ec2-user/me' >> /home/ec2-user/me/deploy.log
-cd /home/ec2-user/me >> /home/ec2-user/me/deploy.log
+echo '/home/ec2-user/freelancing' >> /home/ec2-user/freelancing/deploy.log
+cd /home/ec2-user/freelancing >> /home/ec2-user/freelancing/deploy.log
 
-echo 'npm install' >> /home/ec2-user/me/deploy.log 
-npm install >> /home/ec2-user/me/deploy.log
+echo 'npm install' >> /home/ec2-user/freelancing/deploy.log 
+npm install >> /home/ec2-user/freelancing/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/me/deploy.log
-echo 'pm2 restart all-business-backend' >> /home/ec2-user/me/deploy.log
-pm2 restart me >> /home/ec2-user/me/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/freelancing/deploy.log
+echo 'pm2 restart freelancing' >> /home/ec2-user/freelancing/deploy.log
+pm2 restart me >> /home/ec2-user/freelancing/deploy.log

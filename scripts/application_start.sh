53f1d43ff104e9a6541e3fd2026eeddbfdc453a3
johnvchuma <johnvchuma@gmail.com>
2022-11-24 21:53:42 +0300

    freelancing group

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,4 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/freelancing/deploy.log
 echo 'pm2 restart freelancing' >> /home/ec2-user/freelancing/deploy.log
-pm2 restart me >> /home/ec2-user/freelancing/deploy.log
+pm2 restart freelancing >> /home/ec2-user/freelancing/deploy.log
